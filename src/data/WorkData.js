2dc16004cc8927b8d5ccbb94b6e85b2cf16de24f
Kumar Aryan <aryanharshraj0530@gmail.com>
2023-04-08 14:58:45 +0530

    updated work demo links

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -7,7 +7,7 @@ export const Work = [
         description:"My first portfolio I made with the help of HTML, CSS and Javascript",
         tags:["html","css","javascript"],
         
-        demo:"https://kumararyan0530.github.io/",
+        demo:"https://kumararyan0530.github.io",
         github:"https://github.com/KumarAryan0530/KumarAryan0530.github.io"
     },
     {
@@ -26,7 +26,7 @@ export const Work = [
         description:"Weather app which fetch the real time environment conditions parameters. It is made by using HTML, CSS and Javascript",
         tags:["html","css","javascript","api"],
         
-        // demo:"http://devto-clone-codebucks27.vercel.app/",
+        demo:"http://weatherapplive.netlify.app",
         github:"https://github.com/KumarAryan0530/Weather-APP"
     },
     {
@@ -35,7 +35,7 @@ export const Work = [
         description:"A spotify clone made by using reactJS and is also my first react project I made. Let's Rock!!",
         tags:["react","javascript","html", "css"],
         
-        // demo:"https://react-sidebar.vercel.app/",
+        demo:"https://spotipymusic.netlify.app",
         github:"https://github.com/KumarAryan0530/Spotipy"
        
     },
